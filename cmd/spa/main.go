420319dfd373e74ae96ed3a8f4035487e1279645
socialdistance <pleaseuseurbrain@protonmail.com>
2022-04-07 17:31:47 +0300

    fix9

--- cmd/spa/main.go
+++ cmd/spa/main.go
@@ -18,7 +18,7 @@ import (
 var configFile string
 
 func init() {
-	flag.StringVar(&configFile, "config", "./configs/config.yaml", "Path to configuration file")
+	flag.StringVar(&configFile, "config", "/etc/spa-test/config.yaml", "Path to configuration file")
 }
 
 func main() {
